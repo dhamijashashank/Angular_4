47fd8347c288bb4e8d621595e14e08b114cb77bd
dhamija.shashank <dhamija.shashank@gmail.com>
2017-11-08 22:17:34 +0530

    adding simple form and data binding

--- FormDataBindingapp1/src/app/app.component.ts
+++ FormDataBindingapp1/src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'first-app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'app';
+  version = '4';
+  text_value ='';
 }
